68f302a0c8bc428e432706e4b5564ae1f870eb01
Harin Ramesh <iamharinramesh@gmail.com>
2024-01-14 23:40:48 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+
+fn parse_cnf(cnf: &String) -> Option<Vec<Vec<&str>>> {
+    let clauses: Vec<&str> = cnf.split("&&").collect();
+    if clauses.is_empty() {
+        None
+    }else{
+        let clauses: Vec<Vec<&str>> = clauses.iter().map(|&clause| {
+            clause.trim_start_matches("(")
+                  .trim_end_matches(")")
+                  .split("||")
+                  .collect()
+        }).collect();
+        Some(clauses)
+    }
+}
+
+
+
+fn main() {
+    println!("Enter a CNF: ");
+    let cnf = "(a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)".to_owned().to_lowercase().replace(" ", "");
+    let conjuctions = parse_cnf(&cnf);
+    dbg!(conjuctions);
+}
+
