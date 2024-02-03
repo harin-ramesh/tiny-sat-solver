ae5c7b2d565b93d6a5e6d3c1c42108707bcbebb1
Harin Ramesh <iamharinramesh@gmail.com>
2024-02-03 21:57:26 +0530

    Added command line argument support

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 ### tiny-sat-solver
+
+Input => CNF expression
+OR => ||
+AND => &&
+Complement => '
+Literals => any character
+Group literal => ()
+
+CNF example:
+(a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)
+(a)&&(a')
+
+Example commands
+cargo run -- -c "(a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)"
+cargo run -- -c "(a)&&(a')"
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,53 @@
+use std::env;
 
 use sat_solver::parser::parse_cnf;
 use sat_solver::core::State;
 use sat_solver::algorithms::dpll;
 
 
- 
-
-fn main() {
-    println!("==== SAT SOLVER ====\n");
+fn help() {
+    println!("==== SAT SOLVER HELP ====\n");
     println!("Input => CNF expression");
     println!("OR => ||");
     println!("AND => &&");
     println!("Complement => '");
     println!("Literals => any character");
     println!("Group literal => ()");
-    println!("Example CNF: (a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)");
-    print!("\n\n");
-    println!("Enter a CNF: ");
-    let cnf_str = "(a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)";
-    let cnf_str = "(a)&&(a')";
-    let mut cnf_wrapper = parse_cnf(cnf_str).unwrap();
-    let result = dpll(&mut cnf_wrapper.cnf, &mut cnf_wrapper.literals); 
-    if result == State::SATISFIABLE {
-        println!("SATISFIABLE");
+    print!("\n");
+    println!("CNF example: ");
+    println!("(a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)");
+    println!("(a)&&(a')");
+    print!("\n");
+    println!("Example commands");
+    println!("cargo run -- -c \"(a || b || c) && (a’ || b’|| c) && (a’|| b || c’) && (a || b’ || c’)\"");
+    println!("cargo run -- -c \"(a)&&(a')\"");
+}
+
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+
+    if let Some(flag) = args.next() {
+        if flag == "-c" {
+            if let Some(cnf) = args.next() {
+                let mut cnf_wrapper = parse_cnf(&cnf).unwrap();
+                let result = dpll(&mut cnf_wrapper.cnf, &mut cnf_wrapper.literals); 
+                if result == State::SATISFIABLE {
+                    println!("SATISFIABLE");
+                } else {
+                    println!("UN-SATISFIABLE");
+                }
+            } else {
+                print!("sdfsdfsdF");
+            }
+        } else if flag == "-h" || flag == "-help" {
+            help();   
+        } else {
+            println!("Unexpected argument: {}", flag);
+        }
     } else {
-        println!("UN-SATISFIABLE");
+        println!("Missing argument");
     }
 }
+
